a3358d63b17eb47efb48a4922f7417ee52183d53
JhonatanDavidMottaMedinaADSO <jhonatanmotta07@gmail.com>
2022-11-15 11:41:03 -0500

    Punto 3 Evidencia 2

--- Evidencia 1/main.js
+++ Evidencia 1/main.js
@@ -1,42 +1,21 @@
-addEventListener("DOMContentLoaded", () => {
+addEventListener("DOMContentLoaded", () =>{
 
-    let tabla1 = document.querySelector(".tabla1")
-    let tabla2 = document.querySelector(".tabla2")
-    let boton = document.querySelector("button")
- 
-    boton.addEventListener("click", (e) => {
-        if (e.target.dataset.option == "mostrar") {
-            let P = (2 * 5) < 8
-            let Q = (4 * 6) > (2 * 5)
-            let PorQ = P || Q
-            tabla1.innerHTML = `
-        <tr>
-            <td>${P}</td>
-            <td>${Q}</td>
-            <td>${PorQ}</td>
-        </tr>
-        `
 
-            P = (4 + 5) < 3
-            Q = (5 * 5) + 4 + 25 < 3
-            PandQ = P && Q
-            tabla2.innerHTML = `
-        <tr>
-            <td>${P}</td>
-            <td>${Q}</td>
-            <td>${PandQ}</td>
-        </tr>
-        `
-            e.target.dataset.option = "limpiar"
-            e.target.innerHTML = "limpiar";
-        } else {
-            e.target.dataset.option = "mostrar"
-            e.target.innerHTML = "click";
-            tabla1.innerHTML = "";
-            tabla2.innerHTML = "";
-        }
+    let answer = document.querySelector(".rta")
+    let myForm = document.querySelector("form")
 
+    myForm.addEventListener("submit", (e) =>{
+
+        e.preventDefault();
+
+        let dataInput = Object.fromEntries(new FormData(e.target));
+
+        let confi = Intl.NumberFormat('de-DE', {style: 'currency', currency: 'COP', minimumFractionDigits:0});
+        
+        let pesoCol = Math.round(Number(dataInput.dolares) * 4821.09)
+
+        answer.innerHTML = `${confi.format(pesoCol)}`
 
     })
-
+    
 })
