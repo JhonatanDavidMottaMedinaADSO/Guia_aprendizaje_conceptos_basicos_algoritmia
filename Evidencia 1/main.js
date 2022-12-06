168199aeb078cb19a75d268007c4deb4e2e35f4f
JhonatanDavidMottaMedinaADSO <jhonatanmotta07@gmail.com>
2022-12-06 11:49:35 -0500

    Punto 6 Evidencia 2

--- Evidencia 1/main.js
+++ Evidencia 1/main.js
@@ -1,21 +1,66 @@
-addEventListener("DOMContentLoaded", () =>{
-
-    let myForm = document.querySelector("form")
-    let rta =document.querySelector("span")
-
-    myForm.addEventListener("submit", (e) =>{
+addEventListener("DOMContentLoaded", (e) => {
+    let form = document.querySelector('form')
+    let arr = 180
+    let pech = 1 / 4
+    let cebo = 1 / 4
+    let pimiv = 1 / 4
+    let pimir = 1 / 4
+    let calpoll = 300
+    form.addEventListener("submit", (e) => {
         e.preventDefault();
+        let dataInput = Object.fromEntries(new FormData(e.target))
+        let cantPeople = Number(dataInput.cant)
+        let arroz = arr * cantPeople
+        let pechuga = pech * cantPeople
+        let cebolla = cebo * cantPeople
+        let pimientaverde = pimiv * cantPeople
+        let pimientaroja = pimir * cantPeople
+        let caldopollo = calpoll * cantPeople
 
-    let dataInput = Object.fromEntries(new FormData(e.target));   
 
-    if (dataInput.grados == "f") {
-        let faC = new Decimal((dataInput.valor - 32) * 5/9)
-        rta.innerHTML = `${dataInput.valor}°F a °C son ${faC.toDecimalPlaces(3)}°C`
-    }
-    else {
-        let caF = new Decimal((dataInput.valor * 9/5) + 32)
-        rta.innerHTML = `${dataInput.valor}°C a °F son ${caF.toDecimalPlaces(3)}°F`
-    }
+        let tabla = document.querySelector("table");
+        tabla.innerHTML = `
+            <thead>
+                <tr>
+                    <th>Ingredientes</th>
+                    <th>Cantidad para ${cantPeople} personas</th>
+                    <th>Preparación</th>
+                </tr> 
+            </thead>
+            <tbody>
+                <tr>
+                    <td> Arroz </td>
+                    <td> ${arroz} gr </td>
+                    <td> 1. Se comienza haciendo el sofrito. Se ponchan las cebollas y los pimientos</td>
+                </tr>
+                <tr>
+                    <td> Pechuga </td>
+                    <td> ${pechuga} </td>
+                    <td> 2. Se incorpora al sofrito la pechuga cortada y salpimentada, se dora</td>
+                </tr>
+                <tr>
+                    <td> Cebolla </td>
+                    <td> ${cebolla} </td>
+                    <td> 3. Una vez dorada, se agrega el arroz y se rehoga un poco.</td>
+                </tr>
+                <tr>
+                    <td> Pimienta Verde </td>
+                    <td> ${pimientaverde} </td>
+                    <td> Al rehogarse esto hace que el arroz absorba todo el sabor que soltó la pechuga y las verduras </td>
+                </tr>
+                <tr>
+                    <td> Pimienta Roja </td>
+                    <td> ${pimientaroja} </td>
+                    <td> 4. Se cubre el arroz con el caldo de pollo y se deja cocinar por 18 minutos</td>
+                </tr>
+                <tr>
+                    <td> Caldo de pollo </td> 
+                    <td> ${caldopollo} </td>
+                    <td> 5. Despues de los 18 queda listo y a disfrutar</td>
+                </tr>
+            </tbody>
+                `
+
     })
-    
-})
+
+})  
